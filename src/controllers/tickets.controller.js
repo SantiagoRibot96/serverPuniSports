e337227bb743f5671a2bdbcfc81780ae41b1f943
SantiagoRibot96 <85650377+santiagoribot@users.noreply.github.com>
2024-08-11 01:12:22 -0300

    Integracion con MercadoPago

--- src/controllers/tickets.controller.js
+++ src/controllers/tickets.controller.js
@@ -10,15 +10,14 @@ const productService = new ProductService();
 
 class TicketController {
     async mercadoPago(req, res) {
-        // try {
-        //     const products = req.body.products;
-        //     console.log(products);
-        //     res.status(200);
-        // } catch (error) {
-        //     console.log(error);
-        //     res.status(500)
-        // }
-        console.log("hola");
+        try {
+            const products = req.body.productos;
+            console.log(products);
+            res.status(200);
+        } catch (error) {
+            console.log(error);
+            res.status(500)
+        }
     }
 
     async deleteTicket(req, res) {
